3452dc5a5fc51b020fa5e7f953c611bab034a302
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-20 04:31:06 +0600

    simple update

---
+++ max.js
@@ -0,0 +1,23 @@
+var business = 850;
+var minister = 950;
+var sochib = 550;
+
+var max = Math.max(business, minister, sochib);
+console.log(max);
+
+/*if(business > minister){
+    if(business > sochib){
+        console.log('Business is bigger');
+    }
+    else{
+        console.log('Sochib is bigger');
+    }
+}
+else{
+    if(minister > sochib){
+        console.log('Minister is bigger');
+    }
+    else{
+        console.log('Sochib is bigger');
+    }
+}*/
