b598f9345490680ea1dd045f15b8430d0b2935c2
shahnewaz601 <shahnewaz601@gmail.com>
2021-01-20 15:56:07 +0600

    simple update

--- array-sum.js
+++ array-sum.js
@@ -9,7 +9,12 @@ var sum = 0;
 // console.log('Total of the numbers: ', sum);
 
 
-//Using function
+
+
+
+//===Using function===//
+var numbers = [20, 30, 40, 50, 60, 70, 80, 90];
+var sum = 0;
 
 function getArraySum(numbers){
     for(i = 0; i < num.length; i++){
@@ -18,6 +23,18 @@ function getArraySum(numbers){
     }
     return sum;
 }
-var numbers = [20, 30, 40, 50, 60, 70, 80, 90];
-var result = getArraySum(numbers)
+var result = getArraySum(numbers);
 console.log('Total of the numbers: ', result);
+
+
+
+
+
+//===Another ===///
+// function add(a, b){
+
+//     return a + b;
+
+// }
+
+// console.log(add("adam" + "eve"));
